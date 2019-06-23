163e260248eda32bb6cc2f2c7a395d99adede67c
Samrith Shankar <samrith@thetotem.co>
2019-06-23 02:43:39 +0200

    Basic implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,49 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 
+import {
+  FORMAT,
+  getStatusFromTime,
+  getTimeoutFromStatus,
+  setDiff,
+  getTextFromTimeAndStatus
+} from './util';
+
 interface ReactLiveTimeProps {
-  time: number;
+  time: Date;
+  format?: string;
 }
 
 const ReactLiveTime: FunctionComponent<ReactLiveTimeProps> = ({
-  time: propTime
+  time,
+  format = FORMAT
 }) => {
-  let timeValue;
+  let timeValue: number;
 
   try {
-    timeValue = new Date(propTime);
+    timeValue = new Date(time).getTime();
   } catch (e) {
     throw new Error(e);
   }
 
-  const [time, updateTime] = useState(timeValue);
+  const diffSetter = setDiff(timeValue);
+  let diff = diffSetter();
+
+  const textSetter = getTextFromTimeAndStatus(diff, timeValue, format);
+
+  const [status, setStatus] = useState(getStatusFromTime(diff));
+  const [text, setText] = useState(textSetter(status));
 
   useEffect(() => {
-    // const timer = setTimeout(() => {
-    //   updateTime(time + 1);
-    // });
-    // return () => clearTimeout(timer);
-  }, [time, updateTime]);
+    const timeout: number = getTimeoutFromStatus(status);
+    const timer = setTimeout(() => {
+      diff = diffSetter();
+      setStatus(getStatusFromTime(diff));
+      setText(textSetter(status));
+    }, timeout);
+    return () => clearTimeout(timer);
+  }, [status]);
 
-  return <div>Example Component: {time}</div>;
+  return <div>Example Component: {text}</div>;
 };
 
 export default ReactLiveTime;
