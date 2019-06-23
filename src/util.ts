e729ff7e3ec0d2153ab3a1ba909c6d14123d5799
Samrith Shankar <samrith@thetotem.co>
2019-06-23 04:45:16 +0200

    Added seconds option

--- src/util.ts
+++ src/util.ts
@@ -4,7 +4,7 @@ import { FORMAT, TimeValues, StateValues } from './constants';
 
 export { FORMAT };
 
-export function getStatusFromTime(diff: number): string {
+export function getStatusFromTime(diff: number, showSeconds?: boolean): string {
   if (diff >= TimeValues.DAY) {
     return StateValues.STATIC;
   }
@@ -17,7 +17,7 @@ export function getStatusFromTime(diff: number): string {
     return StateValues.PER_MINUTE;
   }
 
-  return StateValues.FRESH;
+  return showSeconds ? StateValues.PER_SECOND : StateValues.FRESH;
 }
 
 export function getTimeoutFromStatus(status: string): number {
@@ -25,6 +25,10 @@ export function getTimeoutFromStatus(status: string): number {
     default:
     case StateValues.PER_MINUTE: {
       return TimeValues.MINUTE;
+    }
+
+    case StateValues.PER_SECOND: {
+      return TimeValues.SECOND;
     }
 
     case StateValues.PER_HOUR: {
@@ -42,8 +46,14 @@ export function getCoefficientFromTime(status: string, diff: number): number {
       return 0;
     }
 
+    case StateValues.PER_SECOND: {
+      return Math.floor(
+        ((dayDiff % TimeValues.HOUR) % TimeValues.MINUTE) / TimeValues.SECOND
+      );
+    }
+
     case StateValues.PER_MINUTE: {
-      return Math.round(dayDiff / TimeValues.HOUR / TimeValues.MINUTE);
+      return Math.floor((dayDiff % TimeValues.HOUR) / TimeValues.MINUTE);
     }
 
     case StateValues.PER_HOUR: {
@@ -52,45 +62,51 @@ export function getCoefficientFromTime(status: string, diff: number): number {
   }
 }
 
-export function getTextFromTimeAndStatus(
+export function textSetter(
   diff: number,
-  time: number,
+  time: Date | number,
+  status: string,
   format?: string
-): Function {
-  return function(): string {
-    const coeff = getCoefficientFromTime(status, diff);
+): string {
+  const coeff = getCoefficientFromTime(status, diff);
 
-    switch (status) {
-      default:
-      case StateValues.FRESH: {
-        return 'just now';
-      }
-
-      case StateValues.PER_MINUTE: {
-        if (coeff === 1) {
-          return `${coeff} minute ago`;
-        }
-
-        return `${coeff} minutes ago`;
-      }
-
-      case StateValues.PER_HOUR: {
-        if (coeff === 1) {
-          return `${coeff} hour ago`;
-        }
-
-        return `${coeff} hours ago`;
-      }
-
-      case StateValues.STATIC: {
-        return dateFormat(time, format);
-      }
+  switch (status) {
+    default:
+    case StateValues.FRESH: {
+      return 'just now';
     }
-  };
+
+    case StateValues.PER_SECOND: {
+      return pluralize(coeff, 'second', 'ago');
+    }
+
+    case StateValues.PER_MINUTE: {
+      return pluralize(coeff, 'minute', 'ago');
+    }
+
+    case StateValues.PER_HOUR: {
+      return pluralize(coeff, 'hour', 'ago');
+    }
+
+    case StateValues.STATIC: {
+      return dateFormat(time, format);
+    }
+  }
 }
 
-export function setDiff(timeValue: number): Function {
-  return function(): number {
-    return Date.now() - timeValue;
-  };
+export function timeValueSetter(time: Date | number): number {
+  try {
+    return new Date(time).getTime();
+  } catch (e) {
+    throw new Error(e);
+  }
+}
+
+export function diffSetter(time: Date | number): number {
+  const timeValue: number = timeValueSetter(time);
+  return Date.now() - timeValue;
+}
+
+function pluralize(value: number, text: string, rest: string): string {
+  return [value, value === 1 ? text : text + 's', rest].join(' ');
 }
