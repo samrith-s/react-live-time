afad4f942e9a90542f01ba9b9e9c935d463b45df
Samrith Shankar <samrith@thetotem.co>
2019-06-23 02:43:26 +0200

    Change constants to ts

--- src/constants.js
+++
@@ -1,5 +0,0 @@
-export default {
-  MINUTE: 60 * 1000,
-  HOUR: 60 * 60 * 1000,
-  DAY: 24 * 60 * 60 * 1000
-};
---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const FORMAT = 'isoDateTime';
+
+export const TimeValues = {
+  _SECONDS: 10,
+  MINUTE: 60 * 1000,
+  HOUR: 60 * 60 * 1000,
+  DAY: 24 * 60 * 60 * 1000
+};
+
+export const StateValues = {
+  FRESH: 'fresh',
+  PER_MINUTE: 'pmin',
+  PER_HOUR: 'phr',
+  STATIC: 'static'
+};
