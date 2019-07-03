e5bff6af67d764b2bf2c0534d497dd40fa1c0f17
Samrith Shankar <samrith@thetotem.co>
2019-07-04 01:00:16 +0200

    Added default localization

---
+++ src/localization.ts
@@ -0,0 +1,30 @@
+export default {
+  justNow: 'just now',
+  seconds: 'second',
+  minutes: 'minute',
+  hours: 'hour',
+  days: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ],
+  months: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ],
+  meridians: ['am', 'pm']
+};
