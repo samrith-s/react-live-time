8abe4548b629780f46e94113aab1eecd80e8806c
Samrith Shankar <samrith@thetotem.co>
2019-07-04 15:13:15 +0200

    Update footer

--- example/src/Footer.js
+++ example/src/Footer.js
@@ -4,33 +4,6 @@ export default function Footer () {
   return (
     <footer>
       <p>
-        For formatting date, check out{' '}
-        <a
-          href='https://github.com/felixge/node-dateformat#mask-options'
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          mask options
-        </a>{' '}
-        and{' '}
-        <a
-          href='https://github.com/felixge/node-dateformat#named-formats'
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          named formats
-        </a>
-        . Powered by{' '}
-        <a
-          href='https://github.com/felixge/node-dateformat'
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          dateformat
-        </a>
-        .
-      </p>
-      <p>
         Icons made by{' '}
         <a
           href='https://www.flaticon.com/authors/pixel-buddha'
